b8f778f369f89458b8184357859a48ff52884d8e
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2022-08-28 01:38:04 -0400

    multi line comment added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 const l = console.log;
 
 // 1. Write a program to find whether a given year is a leap year or not.
@@ -16,28 +17,28 @@ l(isLeapYear(2004), "answer 1");
 
 // -------------------------------------------------------------------------------------------------
 
-// 2. Write a JavaScript program to convert temperatures to and from Celsius,
-// Fahrenheit.
-// [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in
-// Fahrenheit ]
-// Expected Output :
-// 60°C is 140 °F
-// 45°F is 7.222222222222222°C
-// 3. Write a program to find the factorial of a number.
+/* 2. Write a JavaScript program to convert temperatures to and from Celsius,
+ Fahrenheit.
+ [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in
+ Fahrenheit ]
+ Expected Output :
+ 60°C is 140 °F
+ 45°F is 7.222222222222222°C
+ */
 
 // Answer :
 // =>
 
 /**
- * 
- * @param {*} c 
+ *
+ * @param {*} c
  * @returns degree of Celsius
  */
 const f = (c) => ((c - 32) * 5) / 9;
 
 /**
- * 
- * @param {*} f 
+ *
+ * @param {*} f
  * @returns degree of Fahrenheit
  */
 const c = (f) => (f * 9) / 5 + 32;
@@ -49,10 +50,10 @@ l(f(140), c(60), "answer 2 ");
 // 3. Write a program to find the factorial of a number.
 
 /**
- * 
- * @param {*} num 
+ *
+ * @param {*} num
  * @returns factorial of num
  */
 const factorialOfNum = (num) => (num !== 0 ? num * factorialOfNum(num - 1) : 1);
 
-l(factorialOfNum(5),"answer 3");
+l(factorialOfNum(5), "answer 3");
